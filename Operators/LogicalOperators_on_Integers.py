48e73c4c8ae5d3cdec18a8693c378094fd7286ec
rahul91rathore <Mygithub@1>
2021-05-16 06:31:40 +0530

    Add txt file to include the used concepts in the problem

--- Operators/LogicalOperators_on_Integers.py
+++ Operators/LogicalOperators_on_Integers.py
@@ -3,7 +3,14 @@ b=20
 c=0
 print(a and b)
 print(a or b)
+print(bool(a and b))
 
 print(c and a)
 print(c or a)
+
+#empty string:
+a1=''
+b2=10
+print(a and b) #'' will be printed in the output as per the concept  "if x evaluates to False then x"
+print(bool(a1 and b2))
 
